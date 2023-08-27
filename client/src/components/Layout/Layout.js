95a9a402d6f50ee2a44f6df67e51dcd62d5d3822
Ankur Keshri <ankurkeshri16@gmail.com>
2023-08-28 01:02:12 +0530

    uploaded project

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const Layout = ({children}) => {
+  return (
+    <>
+    <Header/>
+    <div className='content container mt-4'>
+        {children}
+    </div>
+    <Footer/>
+    </>
+  );
+};
+
+export default Layout;
